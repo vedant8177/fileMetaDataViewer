65a37147fccf5d3f3e9cd750f4bce6219f20a455
vedant8177 <vedantsapalkar99@gmail.com>
2023-01-05 20:38:25 +0530

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const upload = require('express-fileupload');
+const port = process.env.PORT || 3000;
 
 var app = express();
 
+app.use(upload());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -11,10 +16,15 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', function (req, res) {
+  let file = req.files.upfile;
+  res.json({
+    "name": file.name,
+    "type": file.mimetype,
+    "size": file.size
+  });
+});
 
-
-
-const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
